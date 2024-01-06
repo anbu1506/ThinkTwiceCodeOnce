88d5cbb6e4e11442458a53797d147add7de17724
anbu1506 <arasu15062004@gmail.com>
2024-01-06 20:59:11 +0530

    github auth

---
+++ app/ui/loginBtn.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { signIn } from "next-auth/react";
+
+export default function LoginBtn() {
+    return (
+        <>
+            <button onClick={() => {
+                signIn();
+                console.log("login")
+            }} className="bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-2 px-4 rounded-md">Login</button>
+        </>
+    );
+}
