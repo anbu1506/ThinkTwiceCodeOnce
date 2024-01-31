69468a4af2f8fbf17ebabff5069926606c8b1a53
anbu1506 <arasu15062004@gmail.com>
2024-01-31 21:35:59 +0530

    inspect ui factor

--- app/ui/inspect.tsx
+++ app/ui/inspect.tsx
@@ -19,9 +19,10 @@ export default async function Inspect({ userId }: { userId: string }) {
     image: "",
   };
   const uploads = await fetchUploads(user.id);
+
   return (
     <div className="flex flex-col md:flex-row md:h-full w-full">
-      <div className=" md:w-1/2 md:h-full flex justify-center items-center md:flex-col">
+      <div className="md:w-1/2 md:h-full flex justify-center items-center md:flex-col">
         <Image
           height={300}
           width={300}
@@ -38,21 +39,19 @@ export default async function Inspect({ userId }: { userId: string }) {
           <p className="text-sm my-5 font-semibold">
             {(await getUserLikes(user.id)) || 0} likes
           </p>
-          {session?.user.id == userId ? (
+          {session?.user.id == userId && (
             <p>
               <Link href={"/home/manageUploads"}>
                 <PencilSquareIcon className="w-6 h-6 text-slate-400" /> Manage
                 Uploads
               </Link>
             </p>
-          ) : (
-            ""
           )}
         </div>
       </div>
-      <div className="md:absolute md:left-1/2  h-full md:w-1/2 p-2 flex flex-col ">
+      <div className="md:w-1/2 p-2 flex flex-col">
         <div className="">
-          <p className="text-white font-bold ">All uploads</p>
+          <p className="text-white font-bold">All uploads</p>
         </div>
         {uploads?.map((upload, index) => {
           return (
